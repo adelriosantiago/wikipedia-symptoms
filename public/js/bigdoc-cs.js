9e34261fdfe692b6a29e4c12a4a90a1d184e719e
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-06 23:19:05 -0500

    Jan

--- public/js/bigdoc-cs.js
+++ public/js/bigdoc-cs.js
@@ -17,6 +17,14 @@
     			return "abc: " + value
     		}
      */
+
+    /*
+    	exampleDiseases = 
+    		cold : "I cough often and have runny, stuffy nose. My troat is sore and congested. I've been sneezing a lot recently and I feel fatigued even when doing usual tasks like going upstairs.",
+    		#pneumonia :  "I am 60 years old. My main symptom is fever and cough and green mucus sometimes tinged with a bit of blood. My heartbeat is faster than usual and feel more tired and weak than usual.", #No longer used as it its symptoms are too difficult to notice...
+    		diabetes : "I constanly feel thirsty, like if I couldn't quench my thirst. By this reason I'm going to the bathroom very often, I go pee like 12 or 15 times a day when I would usually go like 4 times as much. Also, waking up and going to pee in the middle of the night is becoming really usual and annoying for me. As an additional symptom my vision is slightly blurred too."
+    		GERD : "I have a burning sensation in my chest, I describe it like \"fire\" inside my. When eating, I find it more difficult to swallow the food, therefore I eat and immediately drink water, usually cold water because of the \"fire\" feeling... Also I cough a lot and noticed that now I have a bad breath issue."
+     */
     var connectorStr, filter_diseases, jsonOnly, relevantStr;
     connectorStr = ['the', 'and', 'or'];
     relevantStr = ['pain', 'coughing', 'sneezing'];
@@ -30,12 +38,9 @@
       console.log(callUrl);
       $.get(callUrl, function(msg) {
         console.log(msg);
-        if (jsonOnly) {
-          return $("#json").html(JSON.stringify(msg, null, 4));
-        } else {
-          window.wordsMatch = msg.diseases;
-          return generate();
-        }
+        $("#json").html(JSON.stringify(msg, null, 4));
+        window.wordsMatch = msg.diseases;
+        return generate();
       }).error(function(err) {
         return console.log("Error");
       });
@@ -55,18 +60,15 @@
       return filter_diseases();
     });
     $("#dataSwitch").on('click', function(ev) {
-      var refreshRate;
       jsonOnly = !jsonOnly;
       if (jsonOnly) {
         $("#dataSwitch .label").html("Switch to Word Cloud");
         $("#json").show();
-        $("#wordcloud").hide();
-        return refreshRate = 100;
+        return $("#wordcloud").hide();
       } else {
         $("#dataSwitch .label").html('Switch to JSON');
         $("#json").hide();
-        $("#wordcloud").show();
-        return refreshRate = 500;
+        return $("#wordcloud").show();
       }
     });
     return $("input[type=radio], #font, #max").change(function() {
